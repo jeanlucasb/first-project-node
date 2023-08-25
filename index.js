1d378c31be436f710ca3a731d040d561ef3474b6
Jean-Lucas-B <jeanlucasb8@gmail.com>
2023-08-25 15:29:29 -0300

    projeto-finalizado

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const uuid = require("uuid")
 
 const port = 3000
 const app = express()
-app.use(express.json)
+app.use(express.json());
 
 /*
  -Query params => meu site.com/users?nome=jean&age=32   //FILTROS
@@ -17,23 +17,73 @@ app.use(express.json)
 
 */
 
-const users = {}
+const users = [];
 
 
 
 app.get("/users", (request, response) => {
-                                                                         // Destructuring assignment
-    return response.json({users})
+    // Destructuring assignment
+    return response.json(users)
 })
 
 app.post("/users", (request, response) => {
-   
-   const { name, age } = request.body
-console.log(uuid.v4())
-//    const user = { id:uuid.v4(), name, age }
-    
-return response.json({users})
+
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(users)
 })
+
+app.put("/users/:id", (request, response) => {
+    const { id } = request.params
+    const { name, age } = request.body
+
+    const updateUser = { id, name, age }
+
+    const index = users.findIndex (user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ message: "User not found" })
+    }
+        
+    users[index]= updateUser
+    return response.json(updateUser)
+})
+
+app.delete("/users/:id", (request, response) => {
+    const { id } = request.params
+
+    const index = users.findIndex (user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ message: "User not found" })
+    }
+users.splice(index,1)
+    return response.status(204).json()
+})
+
+
+
+
+/*app.put("/users/:id", (request, response) => {
+    const { id } = request.params
+    const { name, age } = request.body
+
+    const updateUser = { id, name, age }
+
+    const index = users.findIndex(user => user.id === id)
+    
+    if(index < 0){
+        return response.status(404).json({ message: "User not found"})
+    }
+   
+    users[index] = updateUser
+
+    return response.json(updateUser)
+})*/
 
 
 
