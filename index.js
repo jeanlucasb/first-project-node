6155c75a6870cc3ff92fb6ee76613bae6bb3099c
Jean-Lucas-B <jeanlucasb8@gmail.com>
2023-08-29 14:03:25 -0300

    finished my first project node

--- index.js
+++ index.js
@@ -15,13 +15,28 @@ app.use(express.json());
  -PUT / PATCH  => Alterar/Atualizar informação no back-end
  -DELETE       => Dletar informação no back-end
 
+ - Middleware => INTERCEPTADOR => Tem o poder de parar ou alterar dados da requisição 
 */
 
 const users = [];
 
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ erro: "User not found" })
+    }
+    request.userIndex = index
+    request.userId = id
+    next()
+}
 
 
 app.get("/users", (request, response) => {
+
+
     // Destructuring assignment
     return response.json(users)
 })
@@ -37,61 +52,28 @@ app.post("/users", (request, response) => {
     return response.status(201).json(users)
 })
 
-app.put("/users/:id", (request, response) => {
-    const { id } = request.params
+app.put("/users/:id", checkUserId, (request, response) => {
     const { name, age } = request.body
+
+    const index = request.userIndex
+    const id = request.userId
 
     const updateUser = { id, name, age }
 
-    const index = users.findIndex (user => user.id === id)
 
-    if(index < 0){
-        return response.status(404).json({ message: "User not found" })
-    }
-        
-    users[index]= updateUser
+
+    users[index] = updateUser
     return response.json(updateUser)
 })
 
-app.delete("/users/:id", (request, response) => {
-    const { id } = request.params
+app.delete("/users/:id", checkUserId, (request, response) => {
+    const index = request.userIndex
 
-    const index = users.findIndex (user => user.id === id)
 
-    if(index < 0){
-        return response.status(404).json({ message: "User not found" })
-    }
-users.splice(index,1)
+    users.splice(index, 1)
+
     return response.status(204).json()
 })
-
-
-
-
-/*app.put("/users/:id", (request, response) => {
-    const { id } = request.params
-    const { name, age } = request.body
-
-    const updateUser = { id, name, age }
-
-    const index = users.findIndex(user => user.id === id)
-    
-    if(index < 0){
-        return response.status(404).json({ message: "User not found"})
-    }
-   
-    users[index] = updateUser
-
-    return response.json(updateUser)
-})*/
-
-
-
-
-
-
-
-
 
 
 
